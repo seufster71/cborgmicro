a7cde33fdfcb2aaf27608910e7094c9697d17319
seufster <seufster@gmail.com>
2017-08-12 15:10:35 -0400

    update for micro service

--- cborgmicro/src/main/java/com/cborgtech/CborgmicroApplication.java
+++ cborgmicro/src/main/java/com/cborgtech/CborgmicroApplication.java
@@ -2,8 +2,10 @@ package com.cborgtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.cborgtech","org.toasthub"})
 public class CborgmicroApplication {
---
+++ cborgmicro/src/main/java/com/cborgtech/service/MicroServiceClientImpl.java
@@ -0,0 +1,47 @@
+package com.cborgtech.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.toasthub.core.general.model.GlobalConstant;
+import org.toasthub.core.general.model.RestRequest;
+import org.toasthub.core.general.model.RestResponse;
+import org.toasthub.core.general.service.MicroServiceClient;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@Component("MicroServiceClient")
+public class MicroServiceClientImpl implements MicroServiceClient {
+
+	@Autowired
+	protected RestTemplateBuilder restTemplateBuilder;
+	
+	@Autowired
+	protected EurekaClient client;
+	
+	@Override
+	public void process(RestRequest request, RestResponse response) {
+		// use remote service
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = client.getNextServerFromEureka((String) request.getParams().get(GlobalConstant.MICROSERVICENAME), false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<RestRequest> entity = new HttpEntity<>(request,headers);
+		StringBuilder url = new StringBuilder();
+		url.append(baseUrl);
+		url.append((String) request.getParams().get(GlobalConstant.MICROSERVICEPATH));
+		ResponseEntity<RestResponse> result = restTemplate.exchange(url.toString(), HttpMethod.POST, entity, RestResponse.class);
+		
+		response = result.getBody();
+	}
+
+}
